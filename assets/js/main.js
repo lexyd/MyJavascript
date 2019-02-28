c4d85afd9a36617396930f0fb4026e42c08def1c
destiny <destiny-mac@destinys-MacBook-Pro.local>
2019-02-28 16:01:54 +0100

    for-loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,91 @@
+
+
+
+
+//
+// var package = prompt('enter your package');
+//
+// switch (package) {
+//   case "Regular":
+//   alert('Account is regular');
+//     break;
+//
+//     case "silver":
+//     alert('Account is Silver');
+//       break;
+//
+//       case "Gold":
+//       alert('Account is Gold');
+//         break;
+//
+//
+//   default:
+//   alert('Account Imnalid');
+//
+// }
+//
 // var amount = prompt('enter amout');
+// getAccountDefinition(amount);
 //
-// if(amount=== 0 || amount<=300000){
-//   alert('REGULAR');
-// }else if (amount=== 301000 || amount<=600000) {
-//   alert('GOLD');
+// function getAccountDefinition(amount){
 //
-// }else if (amount=== 601000 || amount<=800000) {
-//   alert('PLATNIUM');
+//   if(amount=== 0 || amount<=300000){
+//     alert('REGULAR');
+//   }else if (amount=== 301000 || amount<=600000) {
+//     alert('GOLD');
 //
-// }else {
-//   alert('FIGURE NOT WITHIN RANGE');
+//   }else if (amount=== 601000 || amount<=800000) {
+//     alert('PLATNIUM');
+//
+//   }else {
+//     alert('FIGURE NOT WITHIN RANGE');
+//   }
+//
 // }
 
+ //  var score = prompt('Input score');
+ //  getGrade(score);
+ //
+ // function getGrade(score) {
+ //   if (score >= 0 && score <= 29 ) {
+ //     alert('F');
+ //
+ //   }else if (score >= 30 && score<= 39 ) {
+ //     alert('E');
+ //   }else if (score >=  40 && score<= 49 ) {
+ //     alert('D');
+ //   }else if (score >=  50 && score<= 59 ) {
+ //     alert('C');
+ //   }else if (score >=  60 && score<= 69 ) {
+ //     alert('B');
+ //   }else if (score >=  70 && score<= 100 ) {
+ //     alert('A');
+ //   }else {
+ //     alert('SCORE OUT OF RANGE');
+ //   }
+ //
+ // }
 
-var package = prompt('enter your package');
+    // var person{
+    //   name,age,address,gender,email
+    //
+    // }
+    //
+    // var kenny = new person();
+    // kenny.name = 'kenny';
+    // kenny.age = '33';
+    // kenny.gender = 'female';
+    //
+    // console.log(kenny);
 
-switch (package) {
-  case "Regular":
-  alert('Account is regular');
-    break;
+    var name = ['A','B','C','D','T','Y'];
 
-    case "silver":
-    alert('Account is Silver');
-      break;
+    for (var i = 0; i <= name.length - 1; i++) {
+      var result = i % 2;
+      if (result === 0) {
+        console.log(name[i]);
 
-      case "Gold":
-      alert('Account is Gold');
-        break;
+      }else {
 
-
-  default:
-  alert('Account Imnalid');
-
-}
+      }
+    }
